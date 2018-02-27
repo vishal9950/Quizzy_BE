c9555fd9c2bd4acb11227d44a9eb7c3de88f2aec
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-27 23:25:20 +0530

    Modify route POST /ques to include correct answer

--- src/routes/getQuestions.js
+++ src/routes/getQuestions.js
@@ -8,25 +8,37 @@ const handler = (request, reply) => {
       quesid: ques.questionId,
       ques: ques.question,
     }));
-    Models.questions.destroy({ truncate: true }).then(() => {
-      Models.questions.bulkCreate(allQuesMapped).then(() => {
-        Models.options.destroy({ truncate: true }).then(() => {
-          for (let i = 0; i < allQuesJSON.length; i += 1) {
-            const keyValuePair = Object.entries(allQuesJSON[i]);
-            // console.log(keyValuePair);
-            const prom1 = [];
-            for (let j = 2; j < keyValuePair.length; j += 1) {
-              prom1.push(Models.options.create({
-                quesid: keyValuePair[1][1],
-                option: keyValuePair[j][1],
-              }));
+    const prom = [];
+    for (let k = 0; k < allQuesMapped.length; k += 1) {
+      prom.push(rp(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findAnswerById/${allQuesMapped[k].quesid}`).then((corrAns) => {
+        const corrAnsJSON = JSON.parse(corrAns);
+        return corrAnsJSON;
+      }));
+    }
+    Promise.all(prom).then((answers) => {
+      for (let k = 0; k < allQuesMapped.length; k += 1) {
+        allQuesMapped[k].correctans = answers[k].answer;
+      }
+      Models.questions.destroy({ truncate: true }).then(() => {
+        Models.questions.bulkCreate(allQuesMapped).then(() => {
+          Models.options.destroy({ truncate: true }).then(() => {
+            for (let i = 0; i < allQuesJSON.length; i += 1) {
+              const keyValuePair = Object.entries(allQuesJSON[i]);
+              // console.log(keyValuePair);
+              const prom1 = [];
+              for (let j = 2; j < keyValuePair.length; j += 1) {
+                prom1.push(Models.options.create({
+                  quesid: keyValuePair[1][1],
+                  option: keyValuePair[j][1],
+                }));
+              }
+              Promise.all(prom1).then(() => {
+              });
             }
-            Promise.all(prom1).then(() => {
-            });
-          }
+          });
         });
+        reply('Questions Stored in DB!').code(201);
       });
-      reply('Questions Stored in DB!').code(201);
     });
   });
 };
